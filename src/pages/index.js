89c1452ce596845d1fe45db5710a1dc28a6623a8
Mike <mikeramzdesign@gmail.com>
2019-02-10 15:57:05 -0800

    figuring it out using docs from gatsby site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
-import React from "react"
+import React from "react";
 
-export default () => <div style={{ color: `purple`, fontSize: `72px` }}>Hello gatsby!</div>
+export default () => (
+  <div style={{ color: `purple`}}>
+    <h1>Hello Gatsby</h1>
+    <p>What a world</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  
+  </div>
+);
